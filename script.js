d65eedea210a404e9c463a183ca8483ad76ab26b
Guru Prakash Singh <64581597+singhguruprakash@users.noreply.github.com>
2021-06-18 12:07:24 +0530

    Automatic downlod feature

--- script.js
+++ script.js
@@ -1,132 +1,97 @@
-// JavaScript Document
-'use strict';
+window.addEventListener('DOMContentLoaded', main)
 
-/* globals MediaRecorder */
+async function main() {
+  //Selecting dom elements
+  let video = document.querySelector('#preview')
+  let startBtn = document.querySelector('#start')
+  let stopBtn = document.querySelector('#stop')
+  let timer = document.querySelector('#timer')
+  let downloadBtn = document.querySelector('#download')
 
-let mediaRecorder;
-let recordedBlobs;
+  //Default styling
+  downloadBtn.style.display = 'none'
+  stopBtn.setAttribute('class', 'disabled')
 
-const errorMsgElement = document.querySelector('span#errorMsg');
-const recordedVideo = document.querySelector('video#recorded');
-const recordButton = document.querySelector('button#record');
-const playButton = document.querySelector('button#play');
-const downloadButton = document.querySelector('button#download');
-
-
-recordButton.addEventListener('click', () => {
-  if (recordButton.textContent === 'Record') {
-    startRecording();
-  } else {
-    stopRecording();
-  const blob = new Blob(recordedBlobs, {type: 'video/mp4'});
-  const url = window.URL.createObjectURL(blob);
-  const a = document.createElement('a');
-  a.style.display = 'none';
-  a.href = url;
-  a.download = 'test.mp4';
-  document.body.appendChild(a);
-  a.click();
-  setTimeout(() => {
-    document.body.removeChild(a);
-    window.URL.revokeObjectURL(url);
-  }, 100);
- 
-    recordButton.textContent = 'Record';
-    playButton.disabled = false;
-    downloadButton.disabled = false;
+  //Global varaibles
+  let videoStream = null
+  let mediaRecorder = null
+  let recordedBlobs = []
+  let timerInterval = null
+  let startTime = {
+    hr: 0,
+    min: 0,
+    sec: 0,
   }
-});
+  //Video preview
+  videoStream = await navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: true,
+  })
+  video.srcObject = videoStream
 
+  //Video Start Recording
+  startBtn.addEventListener('click', () => {
+    mediaRecorder = new MediaRecorder(videoStream, {
+      mimeType: 'video/webm',
+    })
+    mediaRecorder.addEventListener('dataavailable', (e) => {
+      recordedBlobs.push(e.data)
+    })
 
-playButton.addEventListener('click', () => {
-  const superBuffer = new Blob(recordedBlobs, {type: 'video/webm'});
-  recordedVideo.src = null;
-  recordedVideo.srcObject = null;
-  recordedVideo.src = window.URL.createObjectURL(superBuffer);
-  recordedVideo.controls = true;
-  recordedVideo.play();
-});
+    mediaRecorder.addEventListener('stop', () => {
+      let videoLocal = URL.createObjectURL(
+        new Blob(recordedBlobs, { type: 'video/webm' })
+      )
+      downloadBtn.href = videoLocal
+      downloadBtn.click()
+    })
+    stopBtn.removeAttribute('class', 'disabled')
+    startBtn.setAttribute('class', 'disabled')
+    mediaRecorder.start(1000)
+    timerInterval = setInterval(() => {
+      startTime.sec++
+      if (startTime.sec > 60) {
+        startTime.sec %= 60
+        startTime.min++
+      }
+      if (startTime.min > 60) {
+        startTime.min %= 60
+        startTime.hr++
+      }
+      if (startTime.hr > 24) {
+        startTime = {
+          hr: 0,
+          min: 0,
+          sec: 0,
+        }
+      }
+      updateTimer(timer, startTime)
+    }, 1000)
+  })
 
-
-downloadButton.addEventListener('click', () => {
-  const blob = new Blob(recordedBlobs, {type: 'video/mp4'});
-  const url = window.URL.createObjectURL(blob);
-  const a = document.createElement('a');
-  a.style.display = 'none';
-  a.href = url;
-  a.download = 'test.mp4';
-  document.body.appendChild(a);
-  a.click();
-  setTimeout(() => {
-    document.body.removeChild(a);
-    window.URL.revokeObjectURL(url);
-  }, 100);
-});
-
-function handleDataAvailable(event) {
-  console.log('handleDataAvailable', event);
-  if (event.data && event.data.size > 0) {
-    recordedBlobs.push(event.data);
-  }
-}
-
-function startRecording() {
-  recordedBlobs = [];
-  let options = {mimeType: 'video/webm;codecs=vp9,opus'};
-  try {
-    mediaRecorder = new MediaRecorder(window.stream, options);
-  } catch (e) {
-    console.error('Exception while creating MediaRecorder:', e);
-    errorMsgElement.innerHTML = `Exception while creating MediaRecorder: ${JSON.stringify(e)}`;
-    return;
-  }
-
-  console.log('Created MediaRecorder', mediaRecorder, 'with options', options);
-  recordButton.textContent = 'Stop Recording';
-  playButton.disabled = true;
-  downloadButton.disabled = true;
-  mediaRecorder.onstop = (event) => {
-    console.log('Recorder stopped: ', event);
-    console.log('Recorded Blobs: ', recordedBlobs);
-  };
-  mediaRecorder.ondataavailable = handleDataAvailable;
-  mediaRecorder.start();
-  console.log('MediaRecorder started', mediaRecorder);
-}
-
-function stopRecording() {
-  mediaRecorder.stop();
-}
-
-function handleSuccess(stream) {
-  recordButton.disabled = false;
-  console.log('getUserMedia() got stream:', stream);
-  window.stream = stream;
-
-  const gumVideo = document.querySelector('video#gum');
-  gumVideo.srcObject = stream;
-}
-
-async function init(constraints) {
-  try {
-    const stream = await navigator.mediaDevices.getUserMedia(constraints);
-    handleSuccess(stream);
-  } catch (e) {
-    console.error('navigator.getUserMedia error:', e);
-    errorMsgElement.innerHTML = `navigator.getUserMedia error:${e.toString()}`;
-  }
-}
-
-document.querySelector('button#start').addEventListener('click', async () => {
-  const hasEchoCancellation = document.querySelector('#echoCancellation').checked;
-  const constraints = {
-    audio: {
-      echoCancellation: {exact: hasEchoCancellation}
-    },
-    video: {
-      width: 1280, height: 720
+  //Video Stop recording
+  stopBtn.addEventListener('click', (event) => {
+    if (mediaRecorder) {
+      if (timerInterval) {
+        clearInterval(timerInterval)
+      }
+      startBtn.removeAttribute('class', 'disabled')
+      stopBtn.setAttribute('class', 'disabled')
+      startTime = {
+        hr: 0,
+        min: 0,
+        sec: 0,
+      }
+      mediaRecorder.stop()
     }
-  };
-  console.log('Using media constraints:', constraints);
-  await init(constraints);
-});
+  })
+}
+// Change Timer
+function updateTimer(timer, time) {
+  timer.textContent = timeString(time.min) + ':' + timeString(time.sec)
+}
+function timeString(t) {
+  return t > 9 ? t.toString() : `0${t}`
+}
+
+
